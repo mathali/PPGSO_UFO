8ae4071b4ce9053c957df570a7d33b19c8c342f4
mathali <matej.halinkovic@gmail.com>
2020-12-14 11:32:30 +0100

    phong shader change

--- shader/phong_frag.glsl
+++ shader/phong_frag.glsl
@@ -46,43 +46,33 @@ in vec3 norm;
 out vec4 FragmentColor;
 
 void main() {
-  vec3 LightDir = normalize(LightDirection - FragPosition);
-  vec3 ViewDir = normalize(ViewPosition - FragPosition);
-  vec3 ReflectDir = normalize(reflect(-LightDir, norm));
-  vec3 Spec = pow(max(dot(ViewDir, ReflectDir), 0.0), 2) * Ks;
+  vec3 LDir = normalize(LightDirection - FragPosition);
+  vec3 VDir = normalize(ViewPosition - FragPosition);
+  vec3 RDir = normalize(reflect(-LDir, norm));
+  vec3 Spec = pow(max(dot(VDir, RDir), 0.0), 2) * Ks;
   vec3 Specular = SpecularStrength * Spec ;
 
-  // Compute diffuse lighting
-  //float diffuse_strength_test = 0.01f;
   vec3 diffuse = max(dot(vec4(norm, 0.0f), vec4(LightDirection, 1.0f)), 0.0f) * Kd * diffuse_strength;
 
   vec3 amb =  Ambient * Ka;
 
-  // Lookup the color in Texture on coordinates given by texCoord
-  // NOTE: Texture coordinate is inverted vertically for compatibility with OBJ
   FragmentColor = texture(Texture, vec2(texCoord.x, 1.0 - texCoord.y) + TextureOffset) * vec4((diffuse + amb + Specular), 1.0f);
   FragmentColor.a = 1.0f;
 
   if(BeamFlag){
-    vec3 dir = normalize(BeamPos - FragPosition2);
-    vec3 L = normalize(-BeamDirection);
-    float spotCosine = dot(dir, L);
-    //if(spotCosine >= BeamCutoff){
-      vec3 LightDir = normalize(BeamPos + FragPosition2);
-      vec3 ViewDir = normalize(ViewPosition - FragPosition2);
-      vec3 ReflectDir = reflect(-dir, norm);
-      vec3 Spec = pow(max(dot(ViewDir, ReflectDir), 0.0), 2) * Ks;
+    vec3 LDir = normalize(BeamPos - FragPosition2);
+    float beamSpecStrength = 0.5f;
+    float diffuse_strength = 5000.0f;
+    vec3 VDir = normalize(ViewPosition - FragPosition2);
+    vec3 RDir = normalize(reflect(-LDir, norm));
+    vec3 Spec = pow(max(dot(VDir, RDir), 0.0), 2) * Ks;
 
-      float dist = distance(BeamPos, FragPosition2);
-      float attenuation = (1.0/(10.0f + 100.0f * dist +  5000.0f * dist * dist));
-      vec3 Specular = Spec * attenuation;
+    float dist = distance(BeamPos, FragPosition2);
+    float attenuation = (1.0/(10.0f + 100.0f * dist +  5000.0f * dist * dist));
 
-      // Compute diffuse lighting
-      if(object == 2 || object == 3) float diffuse_strength = 20.0f;
-      float diffuse_strength = 5000.0f;
-      vec3 diffuse = max(dot(vec4(norm, 1.0f), vec4(normalize(dir), 1.0f)), 0.0f) * Kd * attenuation * diffuse_strength;
+    vec3 Specular = Spec * attenuation * beamSpecStrength;
+    vec3 diffuse = max(dot(vec4(norm, 1.0f), vec4(normalize(LDir), 1.0f)), 0.0f) * Kd * attenuation * diffuse_strength;
 
-      FragmentColor += vec4((diffuse) * BeamColor, 1.0f);
-    //}
+    FragmentColor += vec4((diffuse + Specular) * BeamColor, 1.0f);
   }
 }
